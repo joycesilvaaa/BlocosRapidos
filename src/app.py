f40848bfc0f2f9ddde8c7a240bc6155335e6ab4f
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2023-12-28 23:39:45 -0300

    adicionei função que cria a tabela do banco de dados caso nao tenha

--- src/app.py
+++ src/app.py
@@ -5,24 +5,30 @@ from config import db_config
 app = Flask(__name__)
 app.secret_key = 'blocosrapidos'
 
-from rotas import *
-
-#Inicializa a conexão com o banco de dados
+# Initialize the database connection
 db_connection = mysql.connector.connect(**db_config)
 
-#Cria um cursor para interagir com o banco de dados
-cursor = db_connection.cursor()
+def cria_tabelas_db():
+    cursor = db_connection.cursor()
+    comandos = [
+        'USE BLOCOSRAPIDOS;',
+        '''
+        CREATE TABLE IF NOT EXISTS NOTAS(
+            ID_NOTAS INT AUTO_INCREMENT PRIMARY KEY,
+            TITULO_NOTAS VARCHAR(30) NOT NULL,
+            CONTEUDO_NOTAS VARCHAR(1000) NULL,
+            DATA_NOTA  DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+        '''
+    ]
+    for comando in comandos:
+        cursor.execute(comando)
+    db_connection.commit()
 
-#caminho para o arquivo sql
-script_banco = 'banco.sql'
+from rotas import *
 
-#funçao que executa o script
-def criar_banco(script_banco):
-    with open(script_banco, 'r') as script_file:
-        script = script_file.read()
-        cursor.execute(script)
+cria_tabelas_db()
 
 if __name__ == "__main__":
-    criar_banco(script_banco)
     app.run(debug=True)
 
